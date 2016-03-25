34f3e5400ad51887da6cef1629f09700bd2291f5
Tung Phan <tungphan91@gmail.com>
2016-03-26 00:26:33 +0100

    Member management menu implemented

--- app/view/otherViews.py
+++ app/view/otherViews.py
@@ -7,6 +7,10 @@ from view.gen.newPasswordView import Ui_NewPasswordDialog
 from view.gen.externView import Ui_ExternDialog
 from view.gen.addExternView import Ui_AddExternDialog
 from view.gen.closeExternView import Ui_CloseExternDialog
+from view.gen.memberMenuView import Ui_MemberMenuDialog
+from view.gen.newMemberView import Ui_NewMemberDialog
+from view.gen.closeMemberView import Ui_CloseMemberDialog
+from view.gen.moveMemberView import Ui_MoveMemberDialog
 import bcrypt
 from excel import writeToExcel
 import pdb
@@ -40,10 +44,15 @@ def confirmBox(title = None, text = None, info = None):
     msgBox.setIcon(QMessageBox.Warning)
     msgBox.setStandardButtons(QMessageBox.No | QMessageBox.Yes)
     msgBox.setDefaultButton(QMessageBox.No)
-    
+
     for button in msgBox.buttons():
         button.setMinimumWidth(100)
         button.setMinimumHeight(50)
+
+        if button.text() == '&Yes':
+            button.setText('Ja')
+        else:
+            button.setText('Nee')
 
     return msgBox
 
@@ -55,10 +64,16 @@ class LoginView(QDialog, Ui_LoginDialog):
         self.setupUi(self)
         self.setupConnections()
         self.session = session
+        self.initialize()
 
     def setupConnections(self):
         self.buttonBox.accepted.connect(self.verify)
         self.buttonBox.rejected.connect(self.reject)
+
+    def initialize(self):
+        for button in self.buttonBox.buttons():
+            button.setMinimumHeight(50)
+            button.setMinimumWidth(100)
 
     def verify(self):
         hashed = self.session.query(Password).one().password
@@ -83,12 +98,13 @@ class MenuView(QDialog, Ui_MenuDialog):
 
     def setupConnections(self):
         self.btBack.clicked.connect(self.reject)
-        self.btNewList.clicked.connect(self.confirmNewList)
+        self.btNewList.clicked.connect(self.verify)
         self.btPassword.clicked.connect(self.showNewPasswordDialog)
         self.btExternMenu.clicked.connect(self.showExternDialog)
+        self.btMemberMenu.clicked.connect(self.showMemberMenuDialog)
 
 
-    def confirmNewList(self):
+    def verify(self):
         msgBox = confirmBox(
                 'Nieuwe lijst?',
                 'Je gaat nu de huidige lijst sluiten en een nieuwe aanmaken!',
@@ -123,6 +139,9 @@ class MenuView(QDialog, Ui_MenuDialog):
 
     def showExternDialog(self):
         ExternView(self.session).exec_()
+
+    def showMemberMenuDialog(self):
+        MemberMenuView(self.session).exec_()
 
 
 class NewPasswordView(QDialog, Ui_NewPasswordDialog):
@@ -287,3 +306,222 @@ class CloseExternView(QDialog, Ui_CloseExternDialog):
             )
 
             self.accept()
+
+
+class MemberMenuView(QDialog, Ui_MemberMenuDialog):
+    "Members menu for editing and closing"
+    def __init__(self, session):
+        super(MemberMenuView, self).__init__()
+        self.setupUi(self)
+        self.session = session
+        self.setupConnections()
+        self.initialize()
+
+    def setupConnections(self):
+        self.btCancel.clicked.connect(self.reject)
+        self.btCloseMember.clicked.connect(self.showCloseMemberDialog)
+        self.btNewMember.clicked.connect(self.showNewMemberDialog)
+        self.btMoveMember.clicked.connect(self.showMoveMemberDialog)
+
+    def initialize(self):
+        emptyRooms = self.session.query(Room).filter(Room.member == None).filter(Room.id <= 18).all()
+
+        if emptyRooms:
+            self.btNewMember.setEnabled(True)
+            self.btMoveMember.setEnabled(True)
+
+    def showNewMemberDialog(self):
+        newMemberDialog = NewMemberView(self.session)
+        ret = newMemberDialog.exec_()
+
+        if ret:
+            self.accept()
+
+    def showCloseMemberDialog(self):
+        closeMemberDialog = CloseMemberView(self.session)
+        ret = closeMemberDialog.exec_()
+
+        if ret:
+            self.accept()
+
+    def showMoveMemberDialog(self):
+        moveMemberDialog = MoveMemberView(self.session)
+        ret = moveMemberDialog.exec_()
+        
+        if ret:
+            self.accept()
+
+
+class NewMemberView(QDialog, Ui_NewMemberDialog):
+    def __init__(self, session):
+        super(NewMemberView, self).__init__()
+        self.setupUi(self)
+        self.session = session
+        self.setupConnections()
+        self.initialize()
+
+    def setupConnections(self):
+        self.buttonBox.rejected.connect(self.reject)
+        self.buttonBox.accepted.connect(self.verify)
+
+    def verify(self):
+        name = self.tName.text().lstrip().rstrip()
+        item = self.lRooms.currentItem()
+
+        if len(name) > 0:
+            if item:
+                room = item.data(Qt.UserRole)
+                msgBox = confirmBox(
+                    'Nieuw huisgenoot',
+                    '{} wordt toegevoegd als een nieuw huisgenoot voor kamer {}.'.format(name, room.id),
+                    'Is dit juist?'
+                )
+
+                ret = msgBox.exec_()
+
+                if ret == QMessageBox.Yes:
+                    member = Member(name)
+                    member.room = room
+                    self.session.add(member)
+                    self.session.commit()
+
+                    MessageBox(
+                        'Huisgenoot toegevoegd',
+                        '{} is toegevoegd als een nieuw huisgenoot voor kamer {}!'.format(name, room.id),
+                        QMessageBox.Information
+                    )
+
+                self.accept()
+            else:
+                MessageBox(
+                    'Error!',
+                    'Geen kamer geselecteerd!',
+                    QMessageBox.Critical
+                )
+        else:
+            MessageBox(
+                'Error!',
+                'Ongeldige naam! Naam moet minimaal 1 karakter bevatten!',
+                QMessageBox.Critical
+            )
+
+    def initialize(self):
+        emptyRooms = self.session.query(Room).filter(Room.member == None).filter(Room.id <= 18)
+
+        for room in emptyRooms:
+            item = QListWidgetItem('Kamer {}'.format(room.id), self.lRooms)
+            item.setData(Qt.UserRole, room)            
+
+        for button in self.buttonBox.buttons():
+            button.setMinimumHeight(50)
+            button.setMinimumWidth(100)
+
+class CloseMemberView(QDialog, Ui_CloseMemberDialog):
+    def __init__(self, session):
+        super(CloseMemberView, self).__init__()
+        self.setupUi(self)
+        self.session = session
+        self.setupConnections()
+        self.initialize()
+
+    def setupConnections(self):
+        self.btCancel.clicked.connect(self.reject)
+        self.btConfirm.clicked.connect(self.verify)
+
+    def initialize(self):
+        members = self.session.query(Member).filter(Room.member).filter(Room.id <= 18)
+
+        for member in members:
+            item = QListWidgetItem(member.name, self.lMembers)
+            item.setData(Qt.UserRole, member)
+
+
+    def verify(self):
+        item = self.lMembers.currentItem()
+
+        if item:
+            member = item.data(Qt.UserRole)
+            msgBox = confirmBox(
+                'Huisgenoot vertrekt', 
+                'Klopt het dat {} (Kamer {}) uit huis gaat? Hij of zij wordt dan van de lijst afgehaald!'.format(member.name, member.room.id),
+                'DIT IS DUS NIET VOOR INTERN DOORVERHUIZEN!'
+            )
+
+            ret = msgBox.exec_()
+
+            if ret == QMessageBox.Yes:
+                member.room = None
+
+                MessageBox(
+                    'Huisgenoot vertrokken',
+                    '{} is vertrokken en uit het huis!'.format(member.name),
+                    QMessageBox.Information
+                )
+
+                self.accept()
+
+class MoveMemberView(QDialog, Ui_MoveMemberDialog):
+    def __init__(self, session):
+        super(MoveMemberView, self).__init__()
+        self.setupUi(self)
+        self.session = session
+        self.setupConnections()
+        self.initialize()
+
+    def setupConnections(self):
+        self.btCancel.clicked.connect(self.reject)
+        self.btConfirm.clicked.connect(self.verify)
+
+    def initialize(self):
+        members = self.session.query(Member).filter(Room.member).filter(Room.id <= 18)
+        emptyRooms = self.session.query(Room).filter(Room.member == None).filter(Room.id <= 18)
+
+        for member in members:
+            item = QListWidgetItem(member.name, self.lMembers)
+            item.setData(Qt.UserRole, member)
+
+        for room in emptyRooms:
+            item = QListWidgetItem('Kamer {}'.format(room.id), self.lRooms)
+            item.setData(Qt.UserRole, room)
+
+    def verify(self):
+        memberItem = self.lMembers.currentItem()
+        roomItem = self.lRooms.currentItem()
+
+        if memberItem:
+            if roomItem:
+                member = memberItem.data(Qt.UserRole)
+                room = roomItem.data(Qt.UserRole)
+
+                msgBox = confirmBox(
+                    'Interne doorverhuizing', 
+                    'Klopt het dat {} intern gaat doorverhuizen?'.format(member.name),
+                    'Hij of zij zal dan doorverhuizen naar kamer {}.'.format(room.id)
+                )
+
+                ret = msgBox.exec_()
+
+                if ret == QMessageBox.Yes:
+                    member.room = room
+                    self.session.commit()
+
+                    MessageBox(
+                        'Interne doorverhuizing',
+                        '{} is doorverhuisd naar kamer {}!'.format(member.name, room.id),
+                        QMessageBox.Information
+                    )
+
+                    self.accept()
+
+            else:
+                MessageBox(
+                    'Error!',
+                    'Geen kamer geselecteerd!',
+                    QMessageBox.Critical
+                )
+        else:
+            MessageBox(
+                'Error!',
+                'Geen huisgenoot geselecteerd!',
+                QMessageBox.Critical
+            )
